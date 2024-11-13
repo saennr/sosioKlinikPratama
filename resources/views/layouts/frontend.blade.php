c43009177869972f8fda794c798103069493ab16
nfzmhn <zidninurfauzi2203@gmail.com>
2024-11-13 13:59:57 +0700

    update blade navbar

--- resources/views/layouts/frontend.blade.php
+++
@@ -1,9 +0,0 @@
-    {{-- header --}}
-    @include("components.frontend.header")
-    {{-- navbar --}}
-    @include("components.frontend.navbar")
-    {{-- content --}}
-    @yield("content")
-    {{-- footer --}}
-    @include("components.frontend.footer")
-    
---
+++ resources/views/layouts/views.blade.php
@@ -0,0 +1,9 @@
+    {{-- header --}}
+    @include("components.header")
+    {{-- navbar --}}
+    @include("components.navbar")
+    {{-- content --}}
+    @yield("content")
+    {{-- footer --}}
+    @include("components.footer")
+    
